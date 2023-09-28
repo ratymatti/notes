bfd59cb2ee0ea654deaf26bef001f5df982bbd61
Matti Räty <mattiraty@hotmail.com>
2023-09-28 13:24:32 +0300

    refactored Notes -component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { useState } from 'react';
+import Notes from './components/Notes/Notes';
 
 function App() {
-  const [notes, setNotes] = useState();
+  const [notes, setNotes] = useState(['note 1', 'note 2', 'note 3']);
 
   return (
     <div className="App">
       <div className='notes'>
         <h1>Notes</h1>
+        <Notes
+          notes={notes}/>
       </div>
       <div className='current'>
         <h3>current</h3>
--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-export default function Notes() {
-  return (
-    <div>Notes component</div>
-  )
+export default function Notes(props) {
+    const { notes } = props;
+    
+    return (
+        <div className='Notes'>
+            {
+                notes.map((note, index) => (
+                    <h3 key={index}>{note}</h3>
+                ))
+            }
+        </div>
+    )
 }
 
