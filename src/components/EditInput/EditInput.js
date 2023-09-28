e0ad761b2bdf4793814a5ed2d484f52fd6c47697
Matti Räty <mattiraty@hotmail.com>
2023-09-28 17:08:28 +0300

    created EditInput component

---
+++ src/components/EditInput/EditInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export default function EditInput(props) {
+    const { handleSubmit } = props;
+
+    const [content, setContent] = useState();
+
+    function handleContentChange(e) {
+        setContent(e.target.value);
+    }
+
+    function handleContentSubmit(e) {
+        if (e.key === 'Enter') {
+            handleSubmit(content);    
+        }
+    }
+
+    return (
+        <div className='EditInput'>
+            <input
+                type='text'
+                placeholder='placeholder'
+                onChange={handleContentChange}
+                onKeyDown={handleContentSubmit} >
+            </input>
+        </div>
+    )
+}
+
