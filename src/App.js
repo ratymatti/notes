186fa41518c9ad2100303472cf5a46ecc3a5e5e7
Matti Räty <mattiraty@hotmail.com>
2023-09-28 13:03:40 +0300

    deleted unnecessary things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [notes, setNotes] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='notes'>
+        <h1>Notes</h1>
+      </div>
+      <div className='current'>
+        <h3>current</h3>
+      </div>
+
     </div>
   );
 }
