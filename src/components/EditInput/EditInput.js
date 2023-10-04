31343f7a734e2d1c9c6a910ef9a019ccccfca2b3
Matti Räty <mattiraty@hotmail.com>
2023-10-04 12:11:20 +0300

    created textarea saving function and how date is saved

--- src/components/EditInput/EditInput.js
+++ src/components/EditInput/EditInput.js
@@ -20,33 +20,43 @@ export default function EditInput(props) {
     setContent(e.target.value);
   }
 
+  function getDate() {
+    const date = new Date();
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); 
+      const day = String(date.getDate()).padStart(2, '0');
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
+      
+      return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  }
+
   function handleContentSubmit(e) {
     if (e.key === 'Enter') {
-        const date = new Date();
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); 
-        const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
-        
-        const dateString = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
-        
+      const dateString = getDate()
       handleSubmit(content, dateString);
     }
+  }
+
+  function handleSave() {
+    const dateString = getDate();
+    handleSubmit(content, dateString);
   }
 
   return (
     <div className='EditInput'>
       { activeInput === 'content' ?
-            <textarea   
+            <div className='textarea'>
+              <textarea   
                 ref={inputRef}
                 value={content}
                 onChange={handleContentChange}
                 onKeyDown={handleContentSubmit}
                 rows={7}
                 cols={50} ></textarea>
-              
+              <button type='submit' onClick={handleSave} >save</button>  
+            </div>
         : <input
             type='text'
             value={content}
